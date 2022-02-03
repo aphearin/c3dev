0fda25184615db7716d04378128d4d189bd7b42c
Andrew Hearin <ahearin@anl.gov>
2022-02-03 11:58:06 -0600

    Changed column names of galsampler

--- c3dev/galmocks/scripts/make_um_base.py
+++ c3dev/galmocks/scripts/make_um_base.py
@@ -64,8 +64,8 @@ if __name__ == "__main__":
     with h5py.File(args.outname, "w") as hdf:
         for key in keys_to_inherit:
             hdf[key] = um[key][galsampler_res.target_gals_selection_indx]
-        hdf["target_gals_target_halo_ids"] = galsampler_res.target_gals_target_halo_ids
-        hdf["target_gals_source_halo_ids"] = galsampler_res.target_gals_source_halo_ids
+        hdf["target_halo_ids"] = galsampler_res.target_gals_target_halo_ids
+        hdf["source_halo_ids"] = galsampler_res.target_gals_source_halo_ids
 
     t7 = time()
     print("{0:.1f} seconds to write mock to disk".format(t7 - t6))
